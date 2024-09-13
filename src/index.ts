5cc2df655c37081d7e122d544803ae29f2f6062a
Dimmon786 <bezborodko.dmytro@gmail.com>
2024-09-13 22:14:10 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 // eslint-disable-next-line no-console
   console.log('Hello world!');
 
-  const thisIsAnUnsusedVariable = 'Hello world!';
-
-    const thisIsAnUnusedFunction = () => {
-    console.log('Hello world!');
-    };
